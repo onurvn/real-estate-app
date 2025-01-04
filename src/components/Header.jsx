87213aa38262cb6db3d54927bd87a6567de87e6e
Onur Avan <avanonur@hotmail.com>
2025-01-04 22:03:48 +0300

    authentication using clerk

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,22 @@
-import Link from "next/link";
 import React from "react";
+import Link from "next/link";
 import { FaSearch } from "react-icons/fa";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 const Header = () => {
   return (
-    <header className="bg-slate-200 shadow-md">
+    <header className="bg-bgheader shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link href="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-black">realEstate</span>
+            <span className="hover:text-gray-400">Real Estate</span>
           </h1>
         </Link>
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+        <form className="bg-white p-1.5 rounded-lg flex items-center">
           <input
             type="text"
             placeholder="Search..."
-            className="bg-transparent focus:outline-none w-24 sm:w-64"
+            className="text-black focus:outline-none md:w-72"
           />
           <button>
             <FaSearch className="text-slate-600" />
@@ -23,20 +24,19 @@ const Header = () => {
         </form>
         <ul className="flex gap-4">
           <Link href="/">
-            <li className="hidden md:inline text-slate-700 hover:underline">
-              Home
-            </li>
+            <li className="hidden md:inline  hover:underline">Home</li>
           </Link>
           <Link href="/about">
-            <li className="hidden md:inline text-slate-700 hover:underline">
-              About
-            </li>
+            <li className="hidden md:inline  hover:underline">About</li>
           </Link>
-          <Link href="/sign-in">
-            <li className="hidden md:inline text-slate-700 hover:underline">
-              Sign In
-            </li>
-          </Link>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          <SignedOut>
+            <Link href="/sign-in">
+              <li className="hidden md:inline  hover:underline">Sign In</li>
+            </Link>
+          </SignedOut>
         </ul>
       </div>
     </header>
