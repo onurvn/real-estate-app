c5f1299a309760e77a7c58112e8fbaa9e5584672
Onur Avan <avanonur@hotmail.com>
2025-01-05 01:59:53 +0300

    add mongodb and clerk

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -1,5 +1,7 @@
 import { Webhook } from "svix";
 import { headers } from "next/headers";
+import { createOrUpdateUser, deleteUser } from "@/app/lib/actions/user";
+import { clerkClient } from "@clerk/nextjs/server";
 
 export async function POST(req) {
   const SIGNING_SECRET = process.env.SIGNING_SECRET;
@@ -48,18 +50,48 @@ export async function POST(req) {
 
   // Do something with payload
   // For this guide, log payload to console
-  const { id } = evt.data;
-  const eventType = evt.type;
+  const { id } = evt?.data;
+  const eventType = evt?.type;
 
-  if (evt.type === "user.created") {
-    console.log(`User created: ${id}`);
+  if (eventType === "user.created" || eventType === "user.updated") {
+    const { first_name, last_name, image_url, email_addresses } = evt?.data;
+    try {
+      const user = await createOrUpdateUser(
+        id,
+        first_name,
+        last_name,
+        image_url,
+        email_addresses
+      );
+      if (user && eventType === "user.created") {
+        try {
+          await clerkClient.user.updateUserMetadata(id, {
+            publicMetadata: {
+              userMongoId: user.id,
+            },
+          });
+        } catch (error) {
+          console.log("error could not update user metadata", error);
+        }
+      }
+    } catch (error) {
+      console.log("error could not create or update user", error);
+      return new Response("Error: Could not create or update user", {
+        status: 400,
+      });
+    }
   }
-  if (evt.type === "user.updated") {
-    console.log(`User updated: ${id}`);
+
+  if (eventType === "user.deleted") {
+    try {
+      await deleteUser(id);
+    } catch (error) {
+      console.log("error could not delete user ", error);
+      return new Response("Error: Could not delete user", {
+        status: 400,
+      });
+    }
   }
-  if (evt.type === "user.deleted") {
-    console.log(`User deleted: ${id}`);
-  }
-  
+
   return new Response("Webhook received", { status: 200 });
 }
---
+++ src/app/lib/mongodb/mongoose.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+let initialized = false;
+
+export const connect = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (initialized) {
+    console.log("mongodb already connected");
+    return;
+  }
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      dbName: "real-estate",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    initialized = true;
+    console.log("mongodb connected");
+  } catch (error) {
+    console.log("mongodb connection error", error);
+  }
+};
