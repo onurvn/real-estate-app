ba3dafcdb0d56dab4beafd7ee28d76c96701db11
Onur Avan <avanonur@hotmail.com>
2025-01-06 01:19:29 +0300

    complete about and home pages

---
+++ src/app/about/page.jsx
@@ -0,0 +1,30 @@
+export default function About() {
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-white">
+        About Our Real Estate Projects
+      </h1>
+      <p className="mb-4 text-indigo-100">
+        With years of experience in the real estate industry, we are committed
+        to turning your dream living spaces into reality. We combine modern
+        architecture with functionality, ensuring each project is unique and
+        tailored to meet our clients' needs. Our innovative designs not only
+        enhance the quality of life but also promise long-term value.
+      </p>
+      <p className="mb-4 text-indigo-100">
+        We prioritize sustainable and eco-friendly construction practices,
+        designing energy-efficient buildings that are both visually appealing
+        and highly functional. By utilizing high-quality materials and advanced
+        technology, we deliver exceptional results that cater to both residents'
+        comfort and investors' long-term gains.
+      </p>
+      <p className="mb-4 text-indigo-100">
+        Our goal is to create more than just buildings; we aim to provide safe,
+        peaceful, and enriching environments for our clients. Through
+        understanding your needs, we develop custom solutions and collaborate
+        with industry experts to ensure every project exceeds expectations. We
+        are here to bring your vision to life.
+      </p>
+    </div>
+  );
+}
