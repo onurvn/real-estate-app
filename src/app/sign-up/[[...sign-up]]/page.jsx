5547422191057b747bbe95136caff30b35dc0e48
Onur Avan <avanonur@hotmail.com>
2025-01-05 18:31:01 +0300

    add create listing page and update global styles

--- src/app/sign-up/[[...sign-up]]/page.jsx
+++ src/app/sign-up/[[...sign-up]]/page.jsx
@@ -1,9 +1,12 @@
 import { SignUp } from "@clerk/nextjs";
+import React from "react";
 
-export default function Page() {
+const Page = () => {
   return (
     <div className="flex items-center justify-center p-3">
       <SignUp />
     </div>
   );
-}
+};
+
+export default Page;
